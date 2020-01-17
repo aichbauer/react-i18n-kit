47e0f8f477c1bead331b9c2125acc31ba98741b3
Lukas Aichbauer <l.aichbauer@gmail.com>
2020-01-17 17:05:49 +0100

    Refactor: rm console.log

--- test/helper/deepMerge/index.spec.js
+++ test/helper/deepMerge/index.spec.js
@@ -2,8 +2,7 @@ import { dataWithMissingDeParts, deWithEnFallbackParts } from "../../fixtures";
 import { getI18n } from "../../../src/helper";
 
 test("withI18n | state and props | browser languages", () => {
-  console.log(dataWithMissingDeParts);
   const langWithPartsFallback = getI18n(dataWithMissingDeParts, "de", "en");
-  console.log(langWithPartsFallback);
+
   expect(langWithPartsFallback).toEqual(deWithEnFallbackParts);
 });
